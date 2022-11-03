c55457506117c42cd1f08fd24a52e6491388e1ad
TheRealTehu <levex11@gmail.com>
2022-11-03 19:02:01 +0100

    add starter repo

---
+++ src/main/java/org/example/Util.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.List;
+
+public class Util {
+    public static final String animalFile1 = "src/main/resources/animals1.txt";
+    public static final String animalFile2 = "src/main/resources/animals2.txt";
+    public static final String animalFile3 = "src/main/resources/animals3.txt";
+    public static final String animalFile4 = "src/main/resources/animals4.txt";
+    public static final String animalFile5 = "src/main/resources/animals5.txt";
+
+    public static List<String> validAnimals = List.of("parrot", "horse", "chicken", "elephant");
+}
